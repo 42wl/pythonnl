90539eb776e91e76a9b26208a3d35309d05ffd0f
Daniel Calderon <danielecalderonm03102003@gmail.com>
2024-03-09 14:32:52 -0500

    hola

---
+++ DESARROLLO/supermercadorespetitiva.py
@@ -0,0 +1,21 @@
+precio= int(input("valor compra"))
+
+balotaroja= (precio-0.1)
+balotaverde= (precio-0.15)
+balotaazul= (precio-0.2)
+balotaamarilla= (precio-0.5)
+balotanegra= (precio-100)
+if precio > 50000:
+    for balota  in range("que balota obtuvo")
+    if balota == "roja":
+        print(f"su descuento es:{balotaroja}")
+    if  balota == "verde":
+         print(f"su descuento es:{balotaverde}")
+    if  balota == "azul":
+         print(f"su descuento es:{balotaazul}")
+    if  balota == "amarilla":
+         print(f"su descuento es:{balotaamarilla}")
+    if  balota == "negra":
+         print(f"su descuento es:{balotanegra}")
+    "break"
+else: print("no tiene descuento")
