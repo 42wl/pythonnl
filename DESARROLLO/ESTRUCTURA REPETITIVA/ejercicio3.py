977d80f1534506167d4c1771f7b97e349de49bc7
Deiby Buitrago <deibyalejandrobuitrago@gmail.com>
2024-05-11 14:13:59 -0500

    primer comentario

---
+++ DESARROLLO/ESTRUCTURA REPETITIVA/ejercicio3.py
@@ -0,0 +1,1 @@
+#Suponga que se tiene un conjunto de calificaciones de un grupo de 20 alumnos, realizar un algoritmo para calcular el promedio y la calificacion mas alta y mas baja de todo el grupo 
